b8c6705ad56bc5fa83fc5fbd3f2ca6ff3bd5cf9a
Mootez <mootez.saad@medtech.tn>
2023-02-27 14:59:56 -0400

    add multithreaded file deletion

--- utils/io.py
+++ utils/io.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 import glob, os
 
 def read_file(path, encoding='utf8'):
@@ -15,7 +16,16 @@ def write_file(path, content):
     with open(path, 'w') as f:
         f.write(content)
 
-def batch_delete(files_batch):
+def batch_delete(files, batch_size, n_workers = 10):
+    with ThreadPoolExecutor(n_workers) as exe:
+        for i in range(0, len(files), batch_size):
+            filenames = files[i:(i + batch_size)]
+            _ = exe.submit(_batch_delete, filenames)
+
+def _batch_delete(files_batch):
     for file in files_batch:
         os.remove(file)
         print(f'deleted {file}')
+
+def dir_files(dir):
+    return list(glob.glob(os.path.join(dir, '*.code'), recursive=True))
