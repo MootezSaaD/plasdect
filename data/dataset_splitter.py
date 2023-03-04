b29440e5139fcf912e4d316c43f149a6c7901f99
Mootez <mootez.saad@medtech.tn>
2023-03-04 18:28:22 -0400

    add dataset splitter to get csv files as datasets for each smell

---
+++ data/dataset_splitter.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import os, random
+from utils.io import load_files_by_ext
+
+_path = ""
+
+smells_configs = [
+{
+ "folder": "experiment_ComplexConditional",
+ "training": {"Positive": 5000, "Negative": 5000},
+ "eval": {"Positive": 7489 , "Negative": 139460}
+ 
+},
+{
+ "folder": "experiment_ComplexMethod",
+ "training": {"Positive": 4330, "Negative": 4330},
+ "eval": {"Positive": 1856, "Negative": 145437}
+ 
+},
+{
+ "folder": "experiment_FeatureEnvy",
+ "training": {"Positive": 1260, "Negative": 1260},
+ "eval": {"Positive": 528, "Negative":  50000}
+ 
+},
+{
+ "folder": "experiment_MultifacetedAbstraction",
+ "training": {"Positive": 205, "Negative": 205},
+ "eval": {"Positive": 85, "Negative":  50000}
+ 
+}
+]
+
+def _dataset_splitter(config, root_path):
+    """
+    Returns pandas dataframe with column: "file", "label", "split"
+    """
+    # Load positive samples
+    pos = load_files_by_ext(os.path.join(root_path, config["folder"], "Positive"), "symb")
+    # Load negative samples
+    neg = load_files_by_ext(os.path.join(root_path, config["folder"], "Negative"), "symb")
+    # Shuffle each
+    random.shuffle(pos)
+    random.shuffle(neg)
+
+    train_pos = random.sample(pos, config["training"]["Positive"])
+    train_neg = random.sample(neg, config["training"]["Negative"])
+
+    eval_pos = list(set(pos) - set(train_pos))
+    eval_neg = list(set(neg) - set(train_neg))
+
+    df = [(f, 1, "train") for f in train_pos] + [(f, 0, "train") for f in train_neg] + [(f, 1, "eval") for f in eval_pos] + [(f, 0, "eval") for f in eval_neg]
+
+    return pd.DataFrame(df)
+
+
+if __name__ == "__main__":
+    for s in smells_configs:
+        df = _dataset_splitter(s, _path)
+        df.to_csv(os.path.join(_path, s["folder"], '.csv'), index=None)
