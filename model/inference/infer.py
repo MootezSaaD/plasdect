6b23125f0139a509d5d390ff7cb970c3b3faa423
Mootez <mootez.saad@medtech.tn>
2023-04-09 18:44:40 -0300

    make infer.py runnable

--- model/inference/infer.py
+++ model/inference/infer.py
@@ -6,6 +6,7 @@ from utils.graph_construction import utc, ifc
 from data_loader.data_entry import DataEntry
 from inference.classifier import PlastDectClassifier
 import torch
+import argparse
 
 def inference(w2v_path, src_code, weights, method):
     embeddings = Word2Vec.load(w2v_path).wv
@@ -24,4 +25,16 @@ def inference(w2v_path, src_code, weights, method):
         y_hat = torch.nn.functional.softmax(model(graph), dim=1)
         
     return y_hat
-    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--w2v_path', type=str, required=True, help='Pretrained Word2Vec model.')
+    parser.add_argument('--src_code', type=str, required=True, help='Sample src code.')
+    parser.add_argument('--weights', type=str, required=True, help='CPKT File.')
+    parser.add_argument('--method', type=str, required=True, help='ifc or utc.')
+
+    args = parser.parse_args()
+
+    result = inference(args.w2v_path, args.src_code, args.weights, args.method)
+
+    print(result)
